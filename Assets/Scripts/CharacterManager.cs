f293097d945f70782236d2cd817f149c4db4dd12
Trần Nguyễn Hà Lan <halannevergiveup@gmail.com>
2026-04-27 18:02:21 +0700

    Update
    Removed commented-out code and notes from CharacterManager.

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -2,13 +2,6 @@
 
 public class CharacterManager : MonoBehaviour
 {
-    // class cha chung cho cả player và enemy, có thể để các hàm chung ở đây
-    // như là HandleDamage, HandleDeath, v.v.
-
-    // vì ko có playerHealthManager nên sẽ làm thêm CharacterHealthManager, sau đó playerHealthManager sẽ kế thừa từ CharacterHealthManager, và enemyHealthManager cx vậy
-    //[Header("Character")]
-    //[SerializeField] CharacterHealthManager characterHealthManager;
-
     protected virtual void Awake()
     {
         //if (characterHealthManager == null) characterHealthManager = GetComponent<CharacterHealthManager>();
